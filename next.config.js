d409f93d391277cf44b88b959a151152fb14568b
cohealcouk <info@coheal.co.uk>
2026-02-24 00:21:59 +0100

    Fix Vercel deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: '',
-  basePath: '',
 }
 
 module.exports = nextConfig
